b905b14e014d082ee4d3e4a1db7d5c994c74a9c2
Mohamed <155868109+mohamedshehatadev17@users.noreply.github.com>
2024-08-28 15:57:49 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MVC_Final.Models;
+<<<<<<< HEAD
+using MVC_Final.Services;
+=======
 using MVC_Final.Repositories;
+>>>>>>> d576ac289627ee7cbeba60cf9920020457739937
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +15,31 @@ builder.Services.AddDbContext<Context>(
 options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Cs"));
 });
+<<<<<<< HEAD
+builder.Services.AddScoped<IDoctorAdmin, DoctorAdminService>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option =>
+{
+    option.Password.RequiredLength = 6;
+    option.Password.RequireUppercase = false;
+    option.Password.RequireNonAlphanumeric = false;
+    option.Password.RequireDigit = false;
+
+
+
+
+
+}
+ ).AddEntityFrameworkStores<Context>();
+=======
 
 
 builder.Services.AddScoped<IDoctorAdmin, DoctorAdminRepository>();
+>>>>>>> d576ac289627ee7cbeba60cf9920020457739937
 
 var app = builder.Build();
 
